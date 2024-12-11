e01a3d793136e0628e4e95c1e996b4955da354b8
Ledene4egg <ledene4egg@yandex.ru>
2024-12-11 20:44:20 +0300

    KS-1-2 task3

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main() {
+    val length : Long = 40868600000L
+    val age : Int = 27
+    val partOfDay : Double = 0.075
+    val seconds : Int = 6480
+    val partOfYear : Float= 2.0547945E-4f
+    val apogee : Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
