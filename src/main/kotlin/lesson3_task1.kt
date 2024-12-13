1daaaacae6b6d595138c7963a26c8c8ca59e294a
Ledene4egg <ledene4egg@yandex.ru>
2024-12-13 21:49:12 +0300

    KS-3-1 task1

---
+++ src/main/kotlin/lesson3_task1.kt
@@ -0,0 +1,15 @@
+package org.example
+
+fun main() {
+    val personName: String = "Маруся"
+    var greeting = "Добрый день"
+    println("$greeting, $personName")
+    greeting = "Добрый вечер"
+    println("$greeting, $personName")
+
+//    val time: Int = 20
+//    if (time < 17) {
+//        println("$greeting, $personName")
+//    } else
+//        println("$Greeting, $personName")
+}
