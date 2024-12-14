747877308c145d6eca38038a051652544d63a927
Ledene4egg <ledene4egg@yandex.ru>
2024-12-14 11:13:41 +0300

    KS-3-1 task1

--- src/main/kotlin/lesson3_task1.kt
+++ src/main/kotlin/lesson3_task1.kt
@@ -6,10 +6,5 @@ fun main() {
     println("$greeting, $personName")
     greeting = "Добрый вечер"
     println("$greeting, $personName")
-
-//    val time: Int = 20
-//    if (time < 17) {
-//        println("$greeting, $personName")
-//    } else
-//        println("$Greeting, $personName")
 }
+
