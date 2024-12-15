345d72d965e520908acdd314e4581c5385ee86d9
Ledene4egg <ledene4egg@yandex.ru>
2024-12-15 13:15:53 +0300

    KS-2-4 task4

--- src/main/kotlin/lesson2_task4.kt
+++ src/main/kotlin/lesson2_task4.kt
@@ -3,14 +3,12 @@ package org.example
 fun main() {
     val ironOre = 11
     val cristallOre = 7
-    val baff = 0.2f
-    val ironBaff = ironOre * baff
+    val baff = 20
+    val reformatPercent = baff / 100.0f
+    val ironBaff = ironOre * reformatPercent
     val formattedIronBaff = String.format("%.0f", ironBaff)
-    val cristallBaff = cristallOre * baff
+    val cristallBaff = cristallOre * reformatPercent
     val formattedCristallBaff = String.format("%.0f", cristallBaff)
     println("$formattedIronBaff-бафф железной руды")
     println("$formattedCristallBaff-бафф кристаллической руды")
-
-
-
 }
