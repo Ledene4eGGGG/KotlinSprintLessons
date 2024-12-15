5299f76c1eede6c8706e82743c229106fcbc0f56
Ledene4egg <ledene4egg@yandex.ru>
2024-12-15 14:12:05 +0300

    KS-2-3 task3

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -3,9 +3,11 @@ package org.example
 const val MINUTES_IN_HOUR = 60
 
 fun main() {
-    val timeStartMinutes = 9 * MINUTES_IN_HOUR + 39 //579
-    val finishTimeMinutes = 457 + timeStartMinutes //1036
-    val hoursFinishTime = finishTimeMinutes / MINUTES_IN_HOUR //17
+    val hourOfDeparture = 9
+    val minuteOfDeparture = 39
+    val timeTravelInMinutes = 457
+    val finishTimeMinutes = hourOfDeparture * MINUTES_IN_HOUR + minuteOfDeparture + timeTravelInMinutes
+    val hoursFinishTime = finishTimeMinutes / MINUTES_IN_HOUR
     val restMinutesFinishTime = finishTimeMinutes - hoursFinishTime * MINUTES_IN_HOUR
     println("$hoursFinishTime:$restMinutesFinishTime")
 }
