86d8c15733ea4dac9b7e260822c3ea1c5cd0aae3
Ledene4egg <ledene4egg@yandex.ru>
2024-12-16 09:41:51 +0300

    KS-3-5 task5

--- src/main/kotlin/lesson3_task5.kt
+++ src/main/kotlin/lesson3_task5.kt
@@ -3,5 +3,7 @@ package org.example
 fun main() {
     val str = "D2-D4;0"
     val parts = str.split('-',';')
-    println(parts)
+    println("откуда - " + parts[0])
+    println("куда - " + parts[1])
+    println("номер хода - " + parts[2])
 }
