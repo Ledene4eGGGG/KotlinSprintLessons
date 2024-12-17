28a0ab785a78348cde9beb7a4e9ab1cfd1648191
Ledene4egg <ledene4egg@yandex.ru>
2024-12-17 13:57:01 +0300

    KS-4-4 task4

--- src/main/kotlin/lesson4_4.kt
+++ src/main/kotlin/lesson4_4.kt
@@ -1,22 +1,16 @@
 package org.example
 
 fun main() {
-    val dayOfTrain = 6
-    val exercisesForPress = true
-    val exercisesForHands = true
-    val exercisesForBack = false
-    val exercisesForFoots = false
+    var dayOfTrain = 6
+    val isEvenDay = dayOfTrain % 2 == 0
+    val exercisesForPress = !isEvenDay
+    val exercisesForHands = !isEvenDay
+    val exercisesForBack = isEvenDay
+    val exercisesForFoots = isEvenDay
     println(
-        if (dayOfTrain % 2 == 1) {
-                    "Упражнения для рук:    $exercisesForHands\n" +
-                    "Упражнения для ног:    $exercisesForFoots\n" +
-                    "Упражнения для спины:  $exercisesForBack\n" +
-                    "Упражнения для пресса: $exercisesForPress"
-        } else {
-                    "Упражнения для рук:    ${!exercisesForHands}\n" +
-                    "Упражнения для ног:    ${!exercisesForFoots}\n" +
-                    "Упражнения для спины:  ${!exercisesForBack}\n" +
-                    "Упражнения для пресса: ${!exercisesForPress}"
-        }
+        "Упражнения для рук:    $exercisesForHands\n" +
+                "Упражнения для ног:    $exercisesForFoots\n" +
+                "Упражнения для спины:  $exercisesForBack\n" +
+                "Упражнения для пресса: $exercisesForPress"
     )
 }
