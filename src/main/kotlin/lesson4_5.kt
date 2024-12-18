563a9f584afc50888bd4285b47b4e13b503677c3
Ledene4egg <ledene4egg@yandex.ru>
2024-12-18 21:24:40 +0300

    KS-4-5 task5

--- src/main/kotlin/lesson4_5.kt
+++ src/main/kotlin/lesson4_5.kt
@@ -1,19 +1,25 @@
 package org.example
 
+private const val HAS_DAMAGE = true
+private const val TEAM_SIZE_MINIMAL = 50
+private const val TEAM_SIZE_OPTIMAL = 70
+private const val PROVISION_SIZE_MINIMAL = 50
+private const val IS_GOOD_WEATHER = true
+
 fun main() {
     val hasDamage = readln().toBoolean()
     val teamSize = readln().toInt()
     val provisionSize = readln().toInt()
     val isGoodWeather = readln().toBoolean()
-    if (hasDamage == false &&
-        teamSize in 50..70 &&
-        provisionSize > 50
+    if (hasDamage == !HAS_DAMAGE &&
+        teamSize in TEAM_SIZE_MINIMAL..TEAM_SIZE_OPTIMAL &&
+        provisionSize > PROVISION_SIZE_MINIMAL
     ) {
         println("корабль может плыть")
-    } else if (hasDamage == true &&
-        teamSize == 70 &&
-        provisionSize >= 50 &&
-        isGoodWeather == true
+    } else if (hasDamage == HAS_DAMAGE &&
+        teamSize == TEAM_SIZE_OPTIMAL &&
+        provisionSize >= PROVISION_SIZE_MINIMAL &&
+        isGoodWeather == IS_GOOD_WEATHER
     ) {
         println("корабль может плыть")
     } else {
