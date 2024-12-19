0950adf4d123b39b6f6201d3ac57981be2ee0f6f
Ledene4egg <ledene4egg@yandex.ru>
2024-12-19 21:22:21 +0300

    KS-5-4 task4

--- src/main/kotlin/lesson5_4.kt
+++ src/main/kotlin/lesson5_4.kt
@@ -4,15 +4,17 @@ private const val registeredUserName = "Zaphod"
 private const val registeredUserPassword = "PanGalactic"
 
 fun main() {
-    println("Введите имя пользователя")
+    println("Введите имя пользователя:")
     val nameUser = readln()
     if (nameUser != registeredUserName) {
-        println("Пожалуйста зарегистрируйтесь")
+        println("Пользователь не найден, зарегистрируйтесь")
     } else if (nameUser == registeredUserName) {
-        println("Пожалуйста введите пароль")
+        println("Введите пароль:")
+        val passwordUser = readln()
+        if (passwordUser == registeredUserPassword) {
+            println("Добро пожаловать")
+        } else if (passwordUser != registeredUserPassword) {
+            println("Неудача")
+        }
     }
-    val passwordUser = readln()
-    if (passwordUser == registeredUserPassword) {
-        println("Добро пожаловать на наш корабль")
-    } else println("Пароль не подходит")
 }
